6fb73c0ffbe11641e6ac44835ca1a8428bf20640
Mike S Chen <mike.sihong.chen@gmail.com>
2020-07-23 17:33:35 -0400

    added input validation

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const inquirer = require('inquirer');
+const generateREADME = require('./modules/generateREADME.js');
 const fs = require('fs');
 
 console.log("This is a README.md generator for your Github repo.\nPlease follow the prompts to generate a professional README that you can add to your project!\n");
@@ -16,7 +17,7 @@ const questions = [
     },
     {
         type: "input",
-        message: "What is the objective of this project?",
+        message: "What is the objective of this app?",
         name: "objective"
     },
     {
@@ -26,7 +27,7 @@ const questions = [
     },
     {
         type: "input",
-        message: "Provide examples for use of this project",
+        message: "Provide instructions and examples for use of this project",
         name: "usage"
     },
     {
@@ -38,32 +39,53 @@ const questions = [
         type: "input",
         message: "Enter the path of the image.",
         name: "imagePath",
-        when: (response) => response.includeImage
+        when: (response) => response.includeImage,
+        validate: function(path){
+            if(path.indexOf('./')===0){
+                return true;
+            }
+            return 'Enter a valid path (beginning with ./)';
+        },
     },
     {
         type: "input",
         message: "Enter names of contributors to this project.",
-        name: "contributors"
+        name: "contributors",
     },
     {
         type: "input",
         message: "Enter test case code blocks",
-        name: "tests"
+        name: "tests",
     },
     {
         type: "list",
         message: "Choose a license for this project.",
-        choices: ["MIT", "ISC", "GNU", "none"]
+        choices: ["MIT", "ISC", "GNU", "none"],
+        default: 0,
+        name: "license",
     },
     {
         type: "input",
         message: "Enter your Github username",
-        name: "username"
+        name: "username",
+        default: "mc4506",
     },
     {
         type: "input",
         message: "Enter your email address",
+        default: "mike4506@gmail.com",
         name: "email",
-        validate: function(){}
-    }
+        validate: function(input){
+            if (input.indexOf('@')>0){
+                if(input.indexOf('.', input.indexOf('@')+2)>0){
+                    return true;
+                }
+            }
+            return 'Please enter a valid email address';
+        },
+    },
 ];
+
+inquirer.prompt(questions).then(function(response){
+    console.log(response);
+})
--- modules/generateREADME.js
+++ modules/generateREADME.js
@@ -1,7 +1,21 @@
+const response = {
+    title: 'Test Project',
+    description: 'this is a test description',
+    objective: 'to test',
+    installation: '',
+    usage: '',
+    includeImage: true,
+    imagePath: './images/',
+    contributors: '',
+    tests: '',
+    license: 'MIT',
+    username: 'mc4506',
+    email: 'mike4506@gmail.com'
+};
+
+
 const generateREADME = function () {
 
 }
 
-module.export = {
-
-}
+module.export = generateREADME;
